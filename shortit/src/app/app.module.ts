001db8cd9d20f22500edcda40deb9b9fabfa502d
Sugam Karki <sugamkarki7058@gmail.com>
2022-04-02 07:04:17 +0545

    generated two components

--- shortit/src/app/app.module.ts
+++ shortit/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './modules/material.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ShortenComponent } from './components/shorten/shorten.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent, ShortenComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
