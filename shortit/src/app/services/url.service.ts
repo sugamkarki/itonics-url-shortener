5332821f5bcc5e8287bd7e67b4892404d699b4b8
Sugam Karki <sugamkarki7058@gmail.com>
2022-04-02 09:30:13 +0545

    api url

--- shortit/src/app/services/url.service.ts
+++ shortit/src/app/services/url.service.ts
@@ -1,15 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class UrlService {
   constructor(private http: HttpClient) {}
   create(url: string) {
-    return this.http.post('/api/urls', { originalURL: url });
+    return this.http.post(`${environment.API_URL}/urls`, { originalURL: url });
   }
   getOriginalUrl(key: string) {
-    return this.http.get(`api/urls/${key}`);
+    return this.http.get(`${environment.API_URL}/urls/${key}`);
   }
 }
